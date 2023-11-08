a47fc7a3cfc27e3d1740d3793755494e86c253b4
AVeliz <by.veliz.alejandra@gmail.com>
2023-11-08 03:14:18 -0500

    metodos para transaccion

---
+++ src/aplicacionbancaria/Transferencia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacionbancaria;
+
+/**
+ *
+ * @author byvel
+ */
+public class Transferencia extends Transacciones{
+    public void procesarTrasferencia(){
+        
+    }
+}
