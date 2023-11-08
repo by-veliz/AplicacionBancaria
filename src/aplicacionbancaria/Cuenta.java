c5eff06760301f0721e86e68d43ef120862fad85
AVeliz <by.veliz.alejandra@gmail.com>
2023-11-07 21:47:59 -0500

    metodos para cuenta

--- src/aplicacionbancaria/Cuenta.java
+++ src/aplicacionbancaria/Cuenta.java
@@ -9,5 +9,52 @@ package aplicacionbancaria;
  * @author byvel
  */
 public class Cuenta {
+    private String nroCuenta;
+    private String usuario;
+    private String contrasena;
+    private double balance;
+
+    public String getNroCuenta() {
+        return nroCuenta;
+    }
+
+    public void setNroCuenta(String nroCuenta) {
+        this.nroCuenta = nroCuenta;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
     
+    public void cambioContrasena(String contrasena){
+        this.contrasena = contrasena;
+    }
+    
+    public void logIn(){
+        
+    }
+    
+    public void logOut(){
+        
+    }
 }
