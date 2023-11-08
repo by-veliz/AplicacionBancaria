3b8c51754fdecaa616024d4e575e349addc1d615
NICO <NICO@192.168.1.7>
2023-11-07 22:12:31 -0500

    Probando...

--- src/aplicacionbancaria/AplicacionBancaria.java
+++ src/aplicacionbancaria/AplicacionBancaria.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aplicacionbancaria;
 
-/**
- *
- * @author byvel
- */
 public class AplicacionBancaria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Kowalski, opciones!");
         System.out.println("Retirada estrategica");
