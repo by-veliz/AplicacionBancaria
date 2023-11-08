2150150be708720ce56c66c3377e73e05d8e5d20
AVeliz <by.veliz.alejandra@gmail.com>
2023-11-08 05:08:03 -0500

    metodos para transaccion

--- src/aplicacionbancaria/ConsultaCuenta.java
+++ src/aplicacionbancaria/ConsultaCuenta.java
@@ -1,5 +1,13 @@
 package aplicacionbancaria;
 
 public class ConsultaCuenta {
-    
+    public void reporteTransacciones(Cuenta Usuario){
+        
+    }
+    public void reportePrestamos(Cuenta Usuario){
+        
+    }
+    public void reporteEstadoCuenta(Cuenta Usuario){
+        
+    }
 }
--- src/aplicacionbancaria/Prestamos.java
+++ src/aplicacionbancaria/Prestamos.java
@@ -1,65 +1,96 @@
 package aplicacionbancaria;
 
 public class Prestamos {
-    private float prestamoID;
-    private float montoPrestamo;
-    private float TasaInteres;
-    private float BalancePrestamo;
-    private float cuotasPrestamo;
-    private float prestamoPlazo;
+    private String prestamoID;
+    private double montoPrestamo;
+    private double montoCuotas;
+    private double TasaInteres;
+    private double BalancePrestamo;
+    private int cantidadCuotas;
+    private int prestamoPlazo;
 
-    public float getPrestamoID() {
+    public Prestamos(String prestamoID, double montoPrestamo, double TasaInteres, double BalancePrestamo, int cuotasPrestamo, int prestamoPlazo) {
+        this.prestamoID = prestamoID;
+        this.montoPrestamo = montoPrestamo;
+        this.TasaInteres = TasaInteres;
+        this.BalancePrestamo = BalancePrestamo;
+        this.cantidadCuotas = cuotasPrestamo;
+        this.prestamoPlazo = prestamoPlazo;
+    }
+
+    public String getPrestamoID() {
         return prestamoID;
     }
-    public void setPrestamoID(float prestamoID) {
+
+    public void setPrestamoID(String prestamoID) {
         this.prestamoID = prestamoID;
     }
 
-    public float getMontoPrestamo() {
+    public double getMontoPrestamo() {
         return montoPrestamo;
     }
-    public void setMontoPrestamo(float montoPrestamo) {
+
+    public void setMontoPrestamo(double montoPrestamo) {
         this.montoPrestamo = montoPrestamo;
     }
 
-    public float getTasaInteres() {
+    public double getTasaInteres() {
         return TasaInteres;
     }
-    public void setTasaInteres(float TasaInteres) {
+
+    public void setTasaInteres(double TasaInteres) {
         this.TasaInteres = TasaInteres;
     }
 
-    public float getBalancePrestamo() {
+    public double getBalancePrestamo() {
         return BalancePrestamo;
     }
-    public void setBalancePrestamo(float BalancePrestamo) {
+
+    public void setBalancePrestamo(double BalancePrestamo) {
         this.BalancePrestamo = BalancePrestamo;
     }
 
-    public float getCuotasPrestamo() {
-        return cuotasPrestamo;
-    }
-    public void setCuotasPrestamo(float cuotasPrestamo) {
-        this.cuotasPrestamo = cuotasPrestamo;
+    public int getCantidadCuotas() {
+        return cantidadCuotas;
     }
 
-    public float getPrestamoPlazo() {
+    public void setCantidadCuotas(int cantidadCuotas) {
+        this.cantidadCuotas = cantidadCuotas;
+    }
+
+    public int getPrestamoPlazo() {
         return prestamoPlazo;
     }
-    public void setPrestamoPlazo(float prestamoPlazo) {
+
+    public void setPrestamoPlazo(int prestamoPlazo) {
         this.prestamoPlazo = prestamoPlazo;
     }
-    
-    
-    public float calcularCuotasPrestamo(){
-        return cuotasPrestamo;
+
+    public double getMontoCuotas() {
+        return montoCuotas;
+    }
+
+    public void setMontoCuotas(double montoCuotas) {
+        this.montoCuotas = montoCuotas;
     }
     
-    public float pagarPrestamo(){
-        return montoPrestamo;
+    
+    public void calcularCuotasPrestamo(){
+        double interesMensual = TasaInteres / 12 / 100; 
+
+        this.montoCuotas = montoPrestamo * (interesMensual * Math.pow(1 + interesMensual, cantidadCuotas))
+                / (Math.pow(1 + interesMensual,cantidadCuotas) - 1);
+
     }
     
-    public float generarEstadoPrestamo(){
-        return BalancePrestamo;
+    public void pagarPrestamo(Cuenta Usuario){
+        this.BalancePrestamo -= this.montoCuotas;
+        Usuario.setBalance(Usuario.getBalance() - this.montoCuotas);
     }
+    
+    public void generarEstadoPrestamo(){
+        System.out.println("Prestamo ID: " + prestamoID);
+        System.out.println("Monto del Prestamo: $/" + montoPrestamo);
+        System.out.println("Balance del Prestamo: $/" + BalancePrestamo);
+    }               
 }
