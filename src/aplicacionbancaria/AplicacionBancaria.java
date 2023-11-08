fb0e38719ad9b204c3206be97c22f56e4405f37c
AVeliz <by.veliz.alejandra@gmail.com>
2023-11-07 21:25:12 -0500

    Retirada

--- src/aplicacionbancaria/AplicacionBancaria.java
+++ src/aplicacionbancaria/AplicacionBancaria.java
@@ -15,6 +15,7 @@ public class AplicacionBancaria {
      */
     public static void main(String[] args) {
         System.out.println("Kowalski, opciones!");
+        System.out.println("Retirada estrategica");
     }
     
 }
