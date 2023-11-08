e41ed865fb6cd3e62b026ea5a0e6c436a0f496d4
angel <angel@192.168.1.5>
2023-11-08 00:28:22 -0500

    Agregue en prestamo y transacciones

--- src/aplicacionbancaria/Prestamos.java
+++ src/aplicacionbancaria/Prestamos.java
@@ -1,5 +1,51 @@
 package aplicacionbancaria;
 
 public class Prestamos {
+    private float montoPrestamo;
+    private float TasaInteres;
+    private float BalancePrestamo;
+    private float cuotasPrestamo;
+    private float prestamoPlazo;
+
+    public float getMontoPrestamo() {
+        return montoPrestamo;
+    }
+
+    public void setMontoPrestamo(float montoPrestamo) {
+        this.montoPrestamo = montoPrestamo;
+    }
+
+    public float getTasaInteres() {
+        return TasaInteres;
+    }
+
+    public void setTasaInteres(float TasaInteres) {
+        this.TasaInteres = TasaInteres;
+    }
+
+    public float getBalancePrestamo() {
+        return BalancePrestamo;
+    }
+
+    public void setBalancePrestamo(float BalancePrestamo) {
+        this.BalancePrestamo = BalancePrestamo;
+    }
+
+    public float getCuotasPrestamo() {
+        return cuotasPrestamo;
+    }
+
+    public void setCuotasPrestamo(float cuotasPrestamo) {
+        this.cuotasPrestamo = cuotasPrestamo;
+    }
+
+    public float getPrestamoPlazo() {
+        return prestamoPlazo;
+    }
+
+    public void setPrestamoPlazo(float prestamoPlazo) {
+        this.prestamoPlazo = prestamoPlazo;
+    }
+    
     
 }
--- src/aplicacionbancaria/Transacciones.java
+++ src/aplicacionbancaria/Transacciones.java
@@ -1,5 +1,50 @@
 package aplicacionbancaria;
 
 public class Transacciones {
+    private float transaccion;
+    private float cuentaOrigen;
+    private float cuentaDestino;
+    private float montoTransaccion;
+    private float tipoTransaccion;
+
+    public float getTransaccion() {
+        return transaccion;
+    }
+
+    public void setTransaccion(float transaccion) {
+        this.transaccion = transaccion;
+    }
+
+    public float getCuentaOrigen() {
+        return cuentaOrigen;
+    }
+
+    public void setCuentaOrigen(float cuentaOrigen) {
+        this.cuentaOrigen = cuentaOrigen;
+    }
+
+    public float getCuentaDestino() {
+        return cuentaDestino;
+    }
+
+    public void setCuentaDestino(float cuentaDestino) {
+        this.cuentaDestino = cuentaDestino;
+    }
+
+    public float getMontoTransaccion() {
+        return montoTransaccion;
+    }
+
+    public void setMontoTransaccion(float montoTransaccion) {
+        this.montoTransaccion = montoTransaccion;
+    }
+
+    public float getTipoTransaccion() {
+        return tipoTransaccion;
+    }
+
+    public void setTipoTransaccion(float tipoTransaccion) {
+        this.tipoTransaccion = tipoTransaccion;
+    }
     
 }
